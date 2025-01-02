1d472ce4f127168e0d9ab42664aa44add511a054
ridham <134303835+rhythm239@users.noreply.github.com>
2025-01-02 22:08:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-//your code here!
+//your code here!const scrollcontainer = document.getElementById("infi-list");
+let i = 0;
+
+function addItems(){
+  const li = document.createElement("li");
+  li.innerText = `item ${++i}`;
+  scrollcontainer.appendChild(li);
+}
+
+for(let i = 0; i < 10; i++){
+  addItems();
+}
+
+scrollcontainer.addEventListener("scroll", () => {
+  const maxScrollHeight = scrollcontainer.scrollHeight - scrollcontainer.clientHeight;
+  let currentScrollTop = scrollcontainer.scrollTop;
+
+  let threshold = Math.abs(
+    maxScrollHeight - currentScrollTop
+  );
+
+  if (threshold <= 5){
+    for(let i = 0; i < 2; i++){
+      addItems();
+    }
+  }
+});
+
 
